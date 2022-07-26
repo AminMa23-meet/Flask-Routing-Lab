31e370c80ecb727710f031070e79d4ad736fb0ad
AminMa23-meet <dlyzei9000@gmail.com>
2022-07-26 15:47:37 +0300

    Added code to python

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -11,9 +11,22 @@ app = Flask(  # Create a flask app
 @app.route('/')
 def home():
     return render_template("home.html")
-@app.route('/product')
-def product():
-    return render_template("product.html")
+@app.route('/product/pc')
+def Pc():
+    return render_template("product.html",
+    product = "Gaming Pc",img = "Pc.jpeg",desc ="Great Pc",price ="1500")
+@app.route('/product/chair')
+def chair():
+    return render_template("product.html",
+    product = "Gaming Chair",img = "Chair.jpeg",desc ="comfy gaming chair",price ="160")
+    @app.route('/product/mouse')
+def mouse():
+    return render_template("product.html",
+    product = "Gaming Mouse",img = "Mouse.jpeg",desc ="Great Gaming Mouse",price ="70")
+    @app.route('/product/keyboard')
+def keyboard():
+    return render_template("product.html",
+    product = "Gaming KeyBoard",img = "KeyBoard.jpeg",desc ="Gaming KeyBoard",price ="60")
 @app.route('/cart')
 def cart():
     return render_template("cart.html")
